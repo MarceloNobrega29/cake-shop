42c301dd2cc0ae35f39cc9acde490f798db0de69
Marcelo Nóbrega <nobregamf29@hotmail.com>
2025-12-12 20:01:13 -0300

    feat: finalizando GastoResponseDTO

--- src/main/java/cakeshop/dto/GastoResponseDTO.java
+++ src/main/java/cakeshop/dto/GastoResponseDTO.java
@@ -1,4 +1,15 @@
 package cakeshop.dto;
 
-public record GastoResponseDTO() {
-}
+import cakeshop.enums.CategoriaGasto;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public record GastoResponseDTO(
+        Long id,
+        String descricao,
+        BigDecimal valor,
+        CategoriaGasto categoria,
+        LocalDateTime dataCriacao,
+        LocalDateTime dataModificacao
+) {}
