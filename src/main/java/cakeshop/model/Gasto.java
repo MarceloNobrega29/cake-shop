e4ac98f0d2241b024f370c407e2e699e34c1704d
Marcelo Nóbrega <nobregamf29@hotmail.com>
2025-12-12 19:51:18 -0300

    feat: adiciona model das classes Venda e Gasto

--- src/main/java/cakeshop/model/Gasto.java
+++ src/main/java/cakeshop/model/Gasto.java
@@ -1,4 +1,23 @@
 package cakeshop.model;
 
+import cakeshop.enums.CategoriaGasto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Gasto {
+
+    private Long id;
+    private String descricao;
+    private BigDecimal valor;
+    private CategoriaGasto categoria;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataModificacao;
+    
 }
--- src/main/java/cakeshop/model/Venda.java
+++ src/main/java/cakeshop/model/Venda.java
@@ -1,4 +1,27 @@
 package cakeshop.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Venda {
+
+    private Long id;
+    private String sabor;
+    private Integer quantidade;
+    private BigDecimal precoUnitario = new BigDecimal("10.00");
+    private BigDecimal valorTotal;
+    private LocalDateTime dataVenda;
+
+    public void calcularValorTotal() {
+        this.valorTotal = this.precoUnitario
+                .multiply(new BigDecimal(this.quantidade));
+    }
+
 }
