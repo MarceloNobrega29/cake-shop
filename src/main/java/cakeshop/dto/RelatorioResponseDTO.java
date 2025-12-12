799fb8b119be045e59dc6452f6805273ac706dbf
Marcelo Nóbrega <nobregamf29@hotmail.com>
2025-12-12 20:03:11 -0300

    feat: finalizando RelatorioResponseDTO

--- src/main/java/cakeshop/dto/RelatorioResponseDTO.java
+++ src/main/java/cakeshop/dto/RelatorioResponseDTO.java
@@ -1,4 +1,30 @@
 package cakeshop.dto;
 
-public record RelatorioResponseDTO() {
-}
+import cakeshop.enums.CategoriaGasto;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Map;
+
+public record RelatorioResponseDTO(
+
+        Integer totalBolosVendidos,
+        BigDecimal receitaTotalVendas,
+        BigDecimal totalGastos,
+        BigDecimal lucroLiquido,
+        BigDecimal margemLucro,
+
+
+        List<VendaResponseDTO> vendas,
+        List<GastoResponseDTO> gastos,
+
+
+        Map<String, Integer> vendasPorSabor,
+        Map<CategoriaGasto, BigDecimal> gastosPorCategoria,
+
+
+        LocalDate periodoInicio,
+        LocalDate periodoFim
+) {}
+
