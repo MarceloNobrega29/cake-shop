7c63372df6ed0c1399586434144671e8ffd63293
Marcelo Nóbrega <nobregamf29@hotmail.com>
2025-12-12 19:58:55 -0300

    feat: finalizando VendaRequestDTO

--- src/main/java/cakeshop/dto/VendaRequestDTO.java
+++ src/main/java/cakeshop/dto/VendaRequestDTO.java
@@ -1,4 +1,15 @@
 package cakeshop.dto;
 
-public record VendaRequestDTO() {
-}
+import jakarta.validation.constraints.*;
+
+
+public record VendaRequestDTO(
+        @NotBlank(message = "Sabor é obrigatório")
+        @Size(min = 3, max = 50, message = "Sabor deve ter entre 3 e 50 caracteres")
+        String sabor,
+
+        @NotNull(message = "Quantidade é obrigatória")
+        @Min(value = 1, message = "Quantidade deve ser maior que zero")
+        @Max(value = 100, message = "Quantidade máxima é 100")
+        Integer quantidade
+) {}
