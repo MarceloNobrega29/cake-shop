ba22270c43e0820ff781f0228098e41cf515b84f
Marcelo Nóbrega <nobregamf29@hotmail.com>
2025-12-12 20:05:56 -0300

    feat: adiciona enum CategoriaGasto

--- src/main/java/cakeshop/enums/CategoriaGasto.java
+++ src/main/java/cakeshop/enums/CategoriaGasto.java
@@ -1,4 +1,17 @@
 package cakeshop.enums;
 
 public enum CategoriaGasto {
+
+    INGREDIENTES,
+    EMBALAGENS,
+    ALUGUEL,
+    ENERGIA,
+    AGUA,
+    GAS,
+    FUNCIONARIOS,
+    MARKETING,
+    EQUIPAMENTOS,
+    IMPOSTOS,
+    OUTROS;
+
 }
