2065b8bbcc0cba76dc94eb10a88cd04cff49c9b6
Marcelo Nóbrega <nobregamf29@hotmail.com>
2025-12-12 20:08:04 -0300

    refactor: reaorganiza codigo enum CategoriaGasto

--- src/main/java/cakeshop/enums/CategoriaGasto.java
+++ src/main/java/cakeshop/enums/CategoriaGasto.java
@@ -1,17 +1,25 @@
 package cakeshop.enums;
 
 public enum CategoriaGasto {
+    INGREDIENTES("Ingredientes"),
+    EMBALAGENS("Embalagens"),
+    ALUGUEL("Aluguel"),
+    ENERGIA("Energia Elétrica"),
+    AGUA("Água"),
+    GAS("Gás"),
+    FUNCIONARIOS("Funcionários"),
+    MARKETING("Marketing e Publicidade"),
+    EQUIPAMENTOS("Equipamentos e Manutenção"),
+    IMPOSTOS("Impostos e Taxas"),
+    OUTROS("Outros");
 
-    INGREDIENTES,
-    EMBALAGENS,
-    ALUGUEL,
-    ENERGIA,
-    AGUA,
-    GAS,
-    FUNCIONARIOS,
-    MARKETING,
-    EQUIPAMENTOS,
-    IMPOSTOS,
-    OUTROS;
+    private final String descricao;
 
+    CategoriaGasto(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
 }
